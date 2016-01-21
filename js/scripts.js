98e2a9c1e3d3eb3d8f93f9bb0e7564ec478d9920
4rob2 <reasoner55@hotmail.com>
2016-01-21 12:35:31 -0800

    add indentation for js so css backgrounds can be changed and tracked

--- js/scripts.js
+++ js/scripts.js
@@ -9,51 +9,39 @@ $(document).ready(function() {
     var locations = ["galapagos", "hawaii", "africa", "Isle_of_Man", "Antarctica", "Nile"];
 
 
-    if (pan === "nature" && temp === "warm") {
-      $("#galapagos").show();
-      $("#hawaii").show();
-      $("#africa").show();
-    } else if (pan === "nature" && temp === "discovery")
-      {
-        $("#galapagos").show();
-        $("#africa").show();
-        $("#antarctica").show();
-    } else if (pan === "mankind" && temp === "discovery")
-    {
-        $("#antarctica").show();
-        $("#isleofman").show();
-        $("#egypt").show();
-    };
+  if (pan === "nature" && temp === "warm") {
+    $("#galapagos").show();
+    $("#hawaii").show();
+    $("#africa").show();
+} else if (pan === "nature" && temp === "discovery") {
+    $("#antarctica").show();
+    $("#isleofman").show();
+    $("#egypt").show();
+};
     var amenities = ["Animal Rescue Excursion", "Yacht Racing", "Wild Animal Camera Shoot Safari", "Archelogical Dig", "Whale Watching", "Ancient Reenactment", "Fun for the whole family or just that special someone"];
 
-    if (activity === "camera" && time === "action")   {
-      $("#yacht").show();
-      $("#dig").show();
-      $("#rescue").show();
-    } else if (activity === "camera" && time === "relaxing")  {
-      $("#ancient").show();
-      $("#whale").show();
-      $("#safari").show();
-    }
-    else if (activity === "handsOn" && time === "relaxing")
-    {
-      $("#safari").show();
-      $("#ancient").show();
-      $("#whale").show();
-    }
-    else if (activity === "handsOn" && time === "action")
-    {
-      $("#dig").show();
-      $("#yacht").show();
-      $("#rescue").show();
-  };
-    if (relative === "special"){
-      $("#celebrate").show().append();} else
-      {
-      $("#kids").show().append();
+  if (activity === "camera" && time === "action")   {
+    $("#yacht").show();
+    $("#dig").show();
+    $("#rescue").show();
+} else if (activity === "camera" && time === "relaxing")  {
+    $("#ancient").show();
+    $("#whale").show();
+    $("#safari").show();
+} else if (activity === "handsOn" && time === "relaxing") {
+    $("#safari").show();
+    $("#ancient").show();
+    $("#whale").show();
+} else if (activity === "handsOn" && time === "action") {
+    $("#dig").show();
+    $("#yacht").show();
+    $("#rescue").show();
+};
+  if (relative === "special"){
+    $("#celebrate").show().append();
+} else {
+    $("#kids").show().append();
       }
-
-
     event.preventDefault();
-    });
   });
+});
